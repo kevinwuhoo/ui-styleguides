5f35c234e3bdacef8edfc06a1bf5ec6f30d66073
Kevin Wu <me@kevinformatics.com>
2015-04-17 10:27:07 -0700

    Asset hash breaks image links

--- config.rb
+++ config.rb
@@ -25,7 +25,6 @@ activate :directory_indexes
 configure :build do
   activate :minify_css
   activate :minify_javascript
-  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
